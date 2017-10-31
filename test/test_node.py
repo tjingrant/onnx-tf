26b8cf52a78d0020661e22715cd95244fee33141
Tian Jin <tian.jin1@ibm.com>
2017-10-31 10:48:05 -0400

    fix node tests

--- test/test_node.py
+++ test/test_node.py
@@ -84,7 +84,7 @@ class TestNode(unittest.TestCase):
     shape = [1, 1, 40, 40]
     node_def = helper.make_node("AveragePool", ["X"], ["Y"],
       kernel_shape=[1,2],
-      pads=[0, 0], strides=[1,1])
+      pads=[1, 1], strides=[1,1])
     x = self._get_rnd(shape)
     output = run_node(node_def, [x], device=device)
     test_output = np.zeros(shape)
@@ -178,7 +178,31 @@ class TestNode(unittest.TestCase):
                 raise unittest.SkipTest(
                     "Backend doesn't support device {}".format(device))
     node_def = helper.make_node("Conv", ["X", "weights"],
-                                ["Y"])
+                                ["Y"], pads=[1,1,1,1,1,1])
+    x_shape = [1, 5, 4]
+    x = self._get_rnd(x_shape)
+    weight_shape = [3, 5, 2]
+    weights = self._get_rnd(weight_shape)
+    output = run_node(node_def, [x, weights], device=device)
+    out_shape = [x_shape[0], weight_shape[0], x_shape[2]]
+    test_output = np.zeros(out_shape)
+    for b in range(0, x_shape[0]):
+      for m in range(0, weight_shape[0]):
+        for h in range(0, x_shape[2]):
+          v = 0
+          for c in range(0, x_shape[1]):
+            for k in range(h, min(h+weight_shape[2], x_shape[2])):
+              v += x[b][c][k] * weights[m][c][k-h]
+          test_output[b][m][h] = v
+    np.testing.assert_almost_equal(output["Y"], test_output, decimal=5)
+
+  def test_conv_transpose(self):
+    device = "CUDA"
+    if not supports_device(device):
+                raise unittest.SkipTest(
+                    "Backend doesn't support device {}".format(device))
+    node_def = helper.make_node("ConvTranspose", ["X", "weights"],
+                                ["Y"], pads=[1,1,1,1,1,1])
     x_shape = [1, 5, 4]
     x = self._get_rnd(x_shape)
     weight_shape = [5, 3, 2]
@@ -195,30 +219,6 @@ class TestNode(unittest.TestCase):
               v += x[b][c][k] * weights[c][m][k-h]
           test_output[b][m][h] = v
     np.testing.assert_almost_equal(output["Y"], test_output, decimal=5)
-
-  def test_conv_transpose(self):
-    device = "CUDA"
-    if not supports_device(device):
-                raise unittest.SkipTest(
-                    "Backend doesn't support device {}".format(device))
-    node_def = helper.make_node("ConvTranspose", ["X", "weights"],
-                                ["Y"])
-    x_shape = [1, 5, 4]
-    x = self._get_rnd(x_shape)
-    weight_shape = [3, 5, 2]
-    weights = self._get_rnd(weight_shape)
-    output = run_node(node_def, [x, weights], device=device)
-    out_shape = [x_shape[0], weight_shape[0], x_shape[2]]
-    test_output = np.zeros(out_shape)
-    for b in range(0, x_shape[0]):
-      for m in range(0, weight_shape[0]):
-        for h in range(0, x_shape[2]):
-          v = 0
-          for c in range(0, x_shape[1]):
-            for k in range(h, min(h+weight_shape[2], x_shape[2])):
-              v += x[b][c][k] * weights[m][c][k-h]
-          test_output[b][m][h] = v
-    np.testing.assert_almost_equal(output["Y"], test_output)
 
   def test_div(self):
     node_def = helper.make_node("Div", ["X", "Y"], ["Z"], broadcast=1)
